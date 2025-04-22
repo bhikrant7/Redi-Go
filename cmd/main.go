aee8755d6dfd1c3f9b156a26f532a33edc1a5c72
ShaswataG <gshaswata1@gmail.com>
2025-04-22 20:12:03 +0530

    feat: TCP server, parser, deserialization, serialization, write

---
+++ cmd/main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"fmt"
+	"net"
+)
+
+func main() {
+	fmt.Println("Listening on port :6379")
+
+	// Create a new server
+	l, err := net.Listen("tcp", ":6379")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	// Listen for connections
+	conn, err := l.Accept()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	defer conn.Close()
+
+	for {
+		resp := NewResp(conn)
+		value, err := resp.Read()
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		fmt.Println("checkpoint 1")
+		fmt.Println(value)
+
+
+		writer := NewWriter(conn)
+		writer.Write(Value{typ: "string", str: "OK"})
+	}
+}
