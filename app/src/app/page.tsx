9f59a031108a0106622e3d855755610acf322c68
TasRuhani <tasdeequerockz@gmail.com>
2025-04-28 19:24:54 +0530

    Fancy Card Added

--- app/src/app/page.tsx
+++ app/src/app/page.tsx
@@ -1,61 +1,68 @@
 "use client";
 
 import { useMoviesStore } from "@/stores/movies";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { Input } from "@/components/ui/input";
 import { Search, SendHorizontal } from "lucide-react";
-import Image from "next/image";
+import Card from "../components/custom/card";
+// import Image from 'next/image';
 
 export default function Home() {
   const { movies, loading, error, fetchMovies } = useMoviesStore();
+  const [query, setQuery] = useState("");
 
   useEffect(() => {
-    console.log("movies: ", movies);
     fetchMovies();
   }, [fetchMovies]);
 
+  const handleSearch = () => {
+    fetchMovies();
+  };
+
   return (
-    <div className="min-h-screen font-[family-name:var(--font-geist-sans)] bg-gradient-to-b from-[#100719] to-[#1c1a3f] flex items-center justify-center">
-      <main className="flex flex-col items-center justify-center w-full">
-        <h1 className="text-white text-6xl mb-20">
+    <div className="min-h-screen font-geist-sans bg-gradient-to-b from-[#100719] to-[#1c1a3f] bg-fixed bg-no-repeat bg-cover flex items-center justify-center px-4">
+      <main className="flex flex-col items-center justify-center w-full max-w-[1600px]">
+        <h1 className="text-white text-6xl mb-20 text-center">
           <span className="drop-shadow-[0_0_8px_#fff]">GO-REDIS</span>{" "}
           <span className="text-transparent bg-clip-text bg-gradient-to-r from-pink-500 to-purple-500">
             DATABASE TESTING
           </span>
         </h1>
 
-        <div className="relative w-2/4">
-          <div className="p-[5px] rounded-full bg-gradient-to-r from-pink-500 to-purple-500">
+        <div className="relative w-full max-w-[1200px]">
+          <div className="p-[5px] rounded-full bg-gradient-to-r from-pink-500 to-purple-500 relative">
             <Search color="#fff" size={36} className="absolute top-5 left-7" />
             <Input
+              value={query}
+              onChange={(e) => setQuery(e.target.value)}
               className="text-white px-24 py-8 w-full rounded-full bg-[#111014] placeholder-gray-400 border-none !text-2xl"
               placeholder="Search for a Movie"
             />
             <button
-              onClick={fetchMovies}
+              onClick={handleSearch}
               disabled={loading}
-              className="absolute top-2.5 right-3 py-2.5 px-6 rounded-l-xl rounded-r-full bg-[#202020] hover:bg-purple-500 active:bg-purple-300 transition-all duration-300 cursor-pointer">
+              className="absolute top-2.5 right-3 py-2.5 px-6 rounded-l-xl rounded-r-full bg-[#202020] hover:bg-purple-500 active:bg-purple-300 transition-all duration-300 cursor-pointer"
+            >
               <SendHorizontal color="#fff" className="w-8 h-8" />
-            </button>{" "}
+            </button>
           </div>
 
-          {error && <p className="text-red-500 mt-4">{error}</p>}
+          {error && <p className="text-red-500 mt-4 text-center">{error}</p>}
 
-          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 mt-6">
-            {movies?.map((movie) => (
-              <div key={movie.id} className="border p-4 relative">
-                <Image
+          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-10 mt-10">
+            {movies?.map((movie: any) => (
+              <div key={movie.id} className="">
+                <Card movie={movie} />
+                {/* <Image
                   src={movie.posterPath}
                   alt={movie.originalTitle}
                   className="w-full h-40 object-cover"
                   width={200}
                   height={300}
-                />
-                <h2 className="mt-2 font-semibold text-white">{movie.originalTitle}</h2>
-                <Search
-                  color="#fff"
-                  className="absolute top-5 left-8 w-8 h-8"
-                />
+                /> */}
+                <div className="text-white text-center mt-2">
+                  {movie.originalTitle}
+                </div>
               </div>
             ))}
           </div>
