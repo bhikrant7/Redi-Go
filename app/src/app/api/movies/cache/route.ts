695fa8dba3df7d5067138f75bf0bff29cde4908c
ShaswataG <gshaswata1@gmail.com>
2025-04-29 01:28:57 +0530

    refactor: comment out redis

--- app/src/app/api/movies/cache/route.ts
+++ app/src/app/api/movies/cache/route.ts
@@ -20,12 +20,12 @@ export async function GET(req: Request) {
     // const cacheKey = `movies:page:${page}`;
 
     // 1️⃣ Try cache
-    const cached = await client.get(cacheKey)
-    if (cached) {
-      console.log('cache hit');
-      console.log('cached: ', cached);
-      return NextResponse.json({ source: 'cache', data: JSON.parse(cached) })
-    }
+    // const cached = await client.get(cacheKey)
+    // if (cached) {
+    //   console.log('cache hit');
+    //   console.log('cached: ', cached);
+    //   return NextResponse.json({ source: 'cache', data: JSON.parse(cached) })
+    // }
 
     // 2️⃣ Cache miss → fetch from MongoDB
     await connectMongo(); // Connect to MongoDB
@@ -40,15 +40,15 @@ export async function GET(req: Request) {
     console.log('Movies fetched from MongoDB:', movies);
     
     // 3️⃣ Store in Redis (no TTL support yet)
-    const resp = await client.set(cacheKey, JSON.stringify(movies));
-    console.log('response of cache set: ', resp);
+    // const resp = await client.set(cacheKey, JSON.stringify(movies));
+    // console.log('response of cache set: ', resp);
 
     return NextResponse.json({ source: 'api', duration, data: movies })
   } catch (err: any) {
     console.error('error: ', err);
     return NextResponse.json({ error: err.message }, { status: 500 })
   } finally {
-    console.log('quitting client');
-    client.quit()
+    // console.log('quitting client');
+    // client.quit()
   }
 }
