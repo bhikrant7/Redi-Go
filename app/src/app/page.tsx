0d68ec6bd2eb8edf8b553fc202519b00282b55ce
ShaswataG <gshaswata1@gmail.com>
2025-05-03 20:39:49 +0530

    fix: double fetchMoviesByPage

--- app/src/app/page.tsx
+++ app/src/app/page.tsx
@@ -17,8 +17,6 @@ export default function Home() {
     setCurrentPage(page);
     if (isSearching) {
       searchMovies(searchQuery, page);
-    } else {
-      fetchMoviesByPage(page);
     }
   };
 
